719373a5f952ff3909e002556b87f7b3ee326d7b
Elijah Hollar <elijahshollar@students.abtech.edu>
2020-04-04 14:01:58 -0400

    Added database/db-connect-info.php file

--- database/db-connect-info.php
+++ database/db-connect-info.php
@@ -1,6 +1,20 @@
 <?php
 
-$server = "localhost";
-$user = "wbip";
-$pw = "wbip123";
-$db = "test";
+// siteground
+
+if($_SERVER['HTTP_HOST'] == "elijahh.sgedu.site")
+{
+  define('SERVER', 'localhost' );
+  define('USER', 'elijahh3_admin' );
+  define('PW', 'S!j9&DL(13z#' );
+  define('DB', 'elijahh3_test' );
+}
+// local
+
+else
+{
+  define('SERVER', 'localhost' );
+  define('USER', 'wbip' );
+  define('PW', 'wbip123' );
+  define('DB', 'test' );
+}
